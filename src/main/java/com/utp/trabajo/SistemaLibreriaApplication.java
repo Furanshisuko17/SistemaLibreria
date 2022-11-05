ac572b8ddda304235a0ac0173cd3506a1a442f4e
Furanshisuko17 <darkfran555@gmail.com>
2022-11-05 03:22:43 -0500

    Corrección de los cambios de color

--- src/main/java/com/utp/trabajo/SistemaLibreriaApplication.java
+++ src/main/java/com/utp/trabajo/SistemaLibreriaApplication.java
@@ -3,7 +3,9 @@ package com.utp.trabajo;
 //import org.springframework.boot.SpringApplication;
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
 import com.utp.trabajo.gui.view.MainView;
+import java.awt.Color;
 import java.awt.EventQueue;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +19,8 @@ public class SistemaLibreriaApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(SistemaLibreriaApplication.class, args);
-		FlatLightLaf.setup();
+		FlatDarkLaf.setup();
+		FlatSVGIcon.ColorFilter.getInstance().add(Color.black, new Color(90, 90, 90), new Color(175, 177, 179));
 		ConfigurableApplicationContext context = new SpringApplicationBuilder(SistemaLibreriaApplication.class)
 				.headless(false)
 				.web(WebApplicationType.NONE)
