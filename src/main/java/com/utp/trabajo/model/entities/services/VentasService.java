131297682cbbc291c39a4662838000053e2bb63c
Furanshisuko17 <darkfran555@gmail.com>
2022-11-05 03:23:21 -0500

    Implementada primera versión del listado de ventas

--- src/main/java/com/utp/trabajo/model/entities/services/VentasService.java
+++ src/main/java/com/utp/trabajo/model/entities/services/VentasService.java
@@ -1,14 +1,38 @@
 package com.utp.trabajo.model.entities.services;
 
-import com.formdev.flatlaf.extras.FlatSVGIcon;
 import com.utp.trabajo.model.dao.VentaDao;
+import com.utp.trabajo.model.entities.Venta;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class VentasService {
 	
     @Autowired
     private VentaDao ventaDao;
-
+	
+	@Transactional(readOnly = true)
+	public DefaultTableModel listarVentas() {
+		List<Venta> ventas = ventaDao.findAll();
+		
+		DefaultTableModel ventasReturned = new DefaultTableModel();
+		for(Venta venta : ventas) { //TODO: find better way to retrieve all data
+			Object[] values = new Object[9];
+			values[0] = venta.getIdVenta();
+			values[1] = venta.getFechaEmision();
+			values[2] = venta.getComprobante();
+			values[3] = venta.getIgv();
+			values[4] = venta.getMetodoPago();
+			values[5] = venta.getPrecioTotal();
+			values[6] = venta.getEmpleado();
+			values[7] = venta.getCliente();
+			ventasReturned.addRow(values);
+		}
+		
+		return ventasReturned;
+		
+	} 
 }
