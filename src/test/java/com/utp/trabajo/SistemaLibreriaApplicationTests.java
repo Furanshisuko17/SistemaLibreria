f83383ab44febd57d02ce6b4fad1a86f1749b89b
Furanshisuko17 <darkfran555@gmail.com>
2022-11-05 03:19:25 -0500

    Arreglado el error de headlessException en los tests.

--- src/test/java/com/utp/trabajo/SistemaLibreriaApplicationTests.java
+++ src/test/java/com/utp/trabajo/SistemaLibreriaApplicationTests.java
@@ -2,13 +2,18 @@ package com.utp.trabajo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+import com.utp.trabajo.util.HeadlessSpringBootContextLoader;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ContextConfiguration(loader = HeadlessSpringBootContextLoader.class)
 class SistemaLibreriaApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	
 
 
 }
---
+++ src/test/java/com/utp/trabajo/util/HeadlessSpringBootContextLoader.java
@@ -0,0 +1,14 @@
+package com.utp.trabajo.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+
+public class HeadlessSpringBootContextLoader extends SpringBootContextLoader {
+	
+	@Override
+	protected SpringApplication getSpringApplication() {
+		SpringApplication application = super.getSpringApplication();
+		application.setHeadless(false);
+		return application;
+	}
+}
